6fd1d041dcda9f42d73719dc15f3c4152d655e0a
Darcy Quesnel <darcy.quesnel@gmail.com>
2018-09-06 21:09:01 -0400

    Improved formatting.

--- renderer.js
+++ renderer.js
@@ -1,5 +1,7 @@
 // required by index.html
 
+const { shell } = require('electron')
+
 function getSchedule (date, callback) {
   const baseURL = 'http://api.tvmaze.com'
 
@@ -10,7 +12,6 @@ function getSchedule (date, callback) {
 
   var promise = require('bluebird')
   var request = promise.promisifyAll(require('request'), { multiArgs: true })
-
   promise.map(countries, (code) => {
     const url = `${baseURL}/schedule?country=${code}&date=${dateStr}`
     return request.getAsync(url).spread((_, body) => {
@@ -37,13 +38,8 @@ function getShow (name) {
 
   torrentSearch.search(name, 'TV', 1)
     .then(torrents => {
-      const opn = require('opn')
-      // TODO: Search for magnet link?
       if (torrents[0] && torrents[0].magnet) {
-        opn(torrents[0].magnet, { wait: false })
-          .catch((e) => {
-            console.error(e)
-          })
+        shell.openExternal(torrents[0].magnet, { activate: false })
       } else {
         // TODO: Retry?
       }
@@ -75,36 +71,33 @@ getSchedule(new Date(Date.now() - 1 * day), schedule => {
   // TODO: Is jquery worth it with this DOM manipulation?
   // const $ = require('jquery')
   var showList = document.querySelector('#show-list')
+
   var showCard = document.querySelector('template#show-card')
   schedule.forEach(s => {
     var showClone = document.importNode(showCard.content, true)
     showClone.querySelector('#show-img')['src'] = s.show.image ? s.show.image.medium : ''
-    showClone.querySelector('#show-img').addEventListener('click', () => {
-      getShow(`${s.show.name.replace(/[^ \w]/g, '')} ${getSEName(s)} ${resolution}`)
-    })
     showClone.querySelector('#show-name').textContent = s.show.name
     showClone.querySelector('#episode').textContent = `${getSEName(s)}`
     showClone.querySelector('#episode-name').textContent = `${s.name}`
+    showClone.querySelector('#show-imdb-link').addEventListener('click', () => {
+      shell.openExternal(`https://www.imdb.com/title/${s.show.externals.imdb}/`, { activate: false })
+    })
+    showClone.querySelector('#show-download-link').addEventListener('click', () => {
+      getShow(`${s.show.name.replace(/[^ \w]/g, '')} ${getSEName(s)} ${resolution}`)
+    })
 
     var showTags = document.querySelector('template#show-tags')
-
     var countryClone = document.importNode(showTags.content, true)
     countryClone.querySelector('#show-tag').textContent = s.show.network ? `${s.show.network.country.code}` : ''
-    countryClone.querySelector('#show-tag').classList.add('badge-secondary')
     showClone.querySelector('#show-footer').appendChild(countryClone)
-
     if (s.show.type !== 'Scripted') {
       var typeClone = document.importNode(showTags.content, true)
       typeClone.querySelector('#show-tag').textContent = `${s.show.type}`
-      typeClone.querySelector('#show-tag').classList.add('badge-light')
       showClone.querySelector('#show-footer').appendChild(typeClone)
     }
-
     s.show.genres.forEach(tag => {
       var tagClone = document.importNode(showTags.content, true)
       tagClone.querySelector('#show-tag').textContent = `${tag}`
-      tagClone.querySelector('#show-tag').classList.add('badge-light')
-      tagClone.querySelector('#show-tag').classList.add('badge-pill')
       showClone.querySelector('#show-footer').appendChild(tagClone)
     })
 
